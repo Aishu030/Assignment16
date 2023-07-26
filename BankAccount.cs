246334b9c46a7084453e3de7d2cd69b2e853a934
Aishu030 <aishuramesh031@gmail.com>
2023-07-26 15:36:58 +0530

    first commit

---
+++ BankAccount.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment16
+{
+    public class BankAccount
+    {
+        int nextAccno = 10356;
+        int accNo;
+        string accHolderName;
+        double balance;
+
+        public BankAccount(string HolderName)
+        {
+            this.accNo = nextAccno++;
+            this.accHolderName = HolderName;
+            this.balance = 0;
+        }
+        public int AccNo { get { return accNo; } }
+        public string AccHolderName { get {  return accHolderName; } }
+
+        public double Balance { get { return balance; } }
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+                Console.WriteLine($"Deposited {amount} into account {accNo}.\n\nCurrent balance: {balance}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid deposit amount !!!");
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > 0 && amount <= balance)
+            {
+                balance -= amount;
+                Console.WriteLine($"Withdrawn {amount} from account {accNo}. \n\nCurrent balance: {balance}");
+            }
+            else
+            {
+                Console.WriteLine("****Invalid amount or insufficient balance****");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+              BankAccount bAcc = new BankAccount("Aishwarya");
+              Console.WriteLine($"*****Account Details as Follows*****\nAccount holder name :{bAcc.AccHolderName}\nAccount number : {bAcc.AccNo}");
+              Console.WriteLine("Enter the Amount to deposit ");
+              bAcc.Deposit(double.Parse(Console.ReadLine()));
+              Console.WriteLine("Enter the amount to withdraw");
+              bAcc.Withdraw(double.Parse(Console.ReadLine()));
+            Console.ReadKey();
+        }
+    }
+}
